c4ef1934d74dfd98f5e01ab57e373ce7f88cb40c
xxiong93 <xengxiong93@gmail.com>
2020-12-07 20:16:38 -0600

    Add a component folder with a jsx file inside for the app class.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
 const jsxElement = <h1>Our React App</h1>;
 console.log(jsxElement);
 
-ReactDOM.render(jsxElement, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
