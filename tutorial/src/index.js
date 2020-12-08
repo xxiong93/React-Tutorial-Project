e8331fb5569301a8ebd1a5caa772278c65e178fa
xxiong93 <xengxiong93@gmail.com>
2020-12-07 19:02:37 -0600

    create the react app and added in index.js file to src folder

---
+++ tutorial/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsxElement = <h1>Our React App</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(jsxElement, document.getElementById('root'));
