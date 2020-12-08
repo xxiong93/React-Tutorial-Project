80e88c8551b7014cde91e3864cf38da19d3a9987
xxiong93 <xengxiong93@gmail.com>
2020-12-07 22:30:08 -0600

    added in constructor function with books and state variables.  also added in jsx to the render method to display the object properties.  last thing added in was a method to update the index of the books when an onclick event occurs with a button

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,50 @@ import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
 
 class App extends Component {
-    state = { }
+    constructor(props) {
+        super(props);
+        this.books = [
+            {title: "Ready Player One", author: "Ernest Cline"},
+            {title: "All the Light We Cannot See", author: "Anthony Doerr"},
+            {title: "The First and Last Freedom", author: "Jiddu Krishnamurti"},
+        ];
+        this.state = {
+            bookNumber: 0
+        }
+    }
+        goToNextBook(){
+            let tempBookNumber = this.state.bookNumber;
+            tempBookNumber++;
+            console.log(tempBookNumber);
+            if(tempBookNumber === this.books.length){
+                tempBookNumber = 0;
+            }
+            this.setState({
+                bookNumber: tempBookNumber
+                
+            });
+        }
+    
+    
+    
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="row row-spacer">
+                    <div className="col">
+                        {/*Button here to move to the previous book viewed*/}
+                    </div>
+                    <div className="col">
+                        {/*Display book with cover here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col">
+                        {/*Button here to move to the previous book viewed*/}
+                        <button onClick={() => this.goToNextBook()}>Next Book</button>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const jsxElement = <h1>Our React App</h1>;
+const jsxElement = <h1>Our React App</h1>   
 console.log(jsxElement);
 
 ReactDOM.render(<App />, document.getElementById('root'));
